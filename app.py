d699fb3d5f01433f3ad5da70adb254557a6c8340
Alexandre <alexandre_muchinski@hotmail.com>
2023-07-07 09:53:49 +0200

    kepler code with website structure

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ import dash
 import dash_bootstrap_components as dbc
 from dash import Input, Output, dcc, html
 from dash.dependencies import Input, Output, State
+import dash_gif_component as gif
 import pandas as pd
 import plotly.express as px
 import numpy as np
@@ -24,14 +25,12 @@ from keplergl import KeplerGl
 
 from data_processing import process_files
 
-from visualization import create_histogram_plot
+from visualization import *
 
 
-PLOTLY_LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
+PLOTLY_LOGO = "assets/SimMobility_logo.png"
 
-app = dash.Dash(
-    external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME]
-)
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME])
 
 sidebar = html.Div(
     [
@@ -40,7 +39,7 @@ sidebar = html.Div(
                 # width: 3rem ensures the logo is the exact width of the
                 # collapsed sidebar (accounting for padding)
                 html.Img(src=PLOTLY_LOGO, style={"width": "3rem"}),
-                html.H2("Sidebar"),
+                html.H2("Locations"),
             ],
             className="sidebar-header",
         ),
@@ -48,24 +47,29 @@ sidebar = html.Div(
         dbc.Nav(
             [
                 dbc.NavLink(
-                    [html.I(className="fas fa-home me-2"), html.Span("Virtual City")],
+                    [html.I(className="fa fa-home me-2"), html.Span("Home")],
                     href="/",
                     active="exact",
                 ),
                 dbc.NavLink(
-                    [
-                        html.I(className="fas fa-calendar-alt me-2"),
-                        html.Span("Boston"),
-                    ],
-                    href="/calendar",
+                    [html.I(className="fa fa-car me-2"), html.Span("Virtual City")],
+                    href="/Virtual_City",
                     active="exact",
                 ),
                 dbc.NavLink(
                     [
-                        html.I(className="fas fa-envelope-open-text me-2"),
+                        html.I(className="fa fa-taxi me-2"),
+                        html.Span("Boston"),
+                    ],
+                    href="/Boston",
+                    active="exact",
+                ),
+                dbc.NavLink(
+                    [
+                        html.I(className="fa fa-bus me-2"),
                         html.Span("Singapoore"),
                     ],
-                    href="/messages",
+                    href="/Singapore",
                     active="exact",
                 ),
             ],
@@ -77,132 +81,259 @@ sidebar = html.Div(
 )
 
 
-
-
 content = html.Div(id="page-content", className="content")
 
 
-vc_content = html.Div(
+main_page = html.Div(
+    [
+        dbc.Row(dbc.Col(html.H1("Welcome to SimMobility Visualization"))),
+        html.Hr(),
+        dbc.Row(
             [
-                html.H2("Drop your Subtrip.csv files below", className="display-5"),
-                html.P("This is the text container where you can put your text."),
-                
-                dcc.Upload(
-                    [
-                        'Drag and Drop or ',
-                        html.A('Select a File')
-                        ],
-                    style={
-                        'width': '100%',
-                        'height': '60px',
-                        'lineHeight': '60px',
-                        'borderWidth': '1px',
-                        'borderStyle': 'dashed',
-                        'borderRadius': '5px',
-                        'textAlign': 'center'
-                        },
-                    id='upload-data',
-                    multiple=True),
-                html.Div(id='file-list'),
-                html.Button('Process Files', id='transform-button', n_clicks=0),
-                html.Hr(),
-                dcc.Loading(
-                    id='loading',
-                    type='default',
-                    children=[
-                        html.Div(
-                            [
-                                dcc.Dropdown(
-                                    id='color-dropdown',
-                                    options=[
-                                        {'label': 'Education', 'value': 'education_category'},
-                                        {'label': 'Income', 'value': 'range_income'},
-                                        {'label': 'Gender', 'value': 'gender'},
-                                        {'label': 'Age', 'value': 'age'},
-                                        {'label': 'Transport Mode', 'value': 'mode'},
-                                        {'label': 'Trip Count', 'value': 'distr'},
-                                        ],
-                                    value='distr',
-                                    clearable=False,
-                                    style={'display': 'none'}
-                                    )
+                dbc.Col(
+                    html.Div(
+                        [
+                            html.H2("Purpose"),
+                            html.Div(
+                                "The under-construction SimMobility visualization tool developed by DTU Transport Department aims to enhance the analysis and interpretation of simulation data generated by the SimMobility platform. It holds promise for providing valuable insights to researchers and practitioners in the field of urban mobility, contributing to improved transportation planning and optimization in urban environments."
+                            ),
+                        ]
+                    )
+                ),
+                dbc.Col(
+                    html.Div(
+                        [
+                            html.H2("How to Use"),
+                            dcc.Markdown(
+                                """
+                                 Below you will see an example of how to use the tool. The main focus is to optimize the data analysis of your simulation. In this case remember to have the csv version of the file called subtrip_metrics. In case you have any doubts, you can check [here](https://github.com/smart-fm/simmobility-prod/wiki/Mid-Term-Output)
+                                 the output files of the Mid Day process.
+                                 Currently the tool is configured to only view Mid Day data, specifically submetrics, however we will be working to expand the analysis to other generated files.
+                                 """
+                            ),
+                        ]
+                    )
+                ),
+            ]
+        ),
+        html.Hr(),
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.Div(
+                        [
+                            html.H2("Available Simulated Locations"),
+                            html.Div(
+                                [
+                                    html.P(
+                                        "SimMobility Visualization is currently available for the following simulated locations:"
+                                    ),
+                                    dcc.Markdown(
+                                        """
+                                             - Virtual City &#x2714
+                                             - Boston &#x2714
+                                             - Singapore &#x2714
+                                             - Copenhagen (under development) &#x2718
+                                             """
+                                    ),
                                 ]
                             ),
-                        dbc.Row(html.Div(id='visualization-1', style={'display': 'none'})),
-                        dbc.Row(
-                            [
-                                dbc.Col(html.Div("One of three columns")),
-                                dbc.Col(html.Div("One of three columns")),
-                                dbc.Col(html.Div("One of three columns")),
-                                ]
+                        ]
+                    )
+                ),
+                dbc.Col(
+                    html.Div(
+                        [
+                            html.H2("Outputs Available to Visualize:"),
+                            html.P(
+                                "SimMobility has several output files generated by each simulation, so far the files that are fully functional are:"
                             ),
-                        dbc.Row(dbc.Col(html.Div("A single column"))),
-                        dbc.Row(dbc.Col(html.Div("A single column"))),
-                        dbc.Row(
-                            [
-                                dbc.Col(html.Div("One of three columns")),
-                                dbc.Col(html.Div("One of three columns")),
-                                dbc.Col(html.Div("One of three columns")),
-                                ]
-                            )
-                        ],
-                    ),
-                html.Div(id='alert-message'),
-                ],
-            id="page-content",className="vc-content"
+                            dcc.Markdown(
+                                """
+                                            - subtrip_metrics.csv &#x2714
+                                            """
+                            ),
+                        ]
+                    )
+                ),
+            ]
+        ),
+        html.Hr(),
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.Div(
+                        [
+                            html.H2("Use Guide"),
+                            html.P(
+                                "The goal of SimMobility Visualization is to be very simple for the User, just upload your submetric files press the button and visualize!"
+                            ),
+                            gif.GifPlayer(
+                                gif="assets/Sim_User_guide.gif",
+                                still="assets/my-still.png",
+                            ),
+                        ]
+                    )
+                )
+            ]
+        ),
+    ],
+)
 
+
+
+dropdown_options_histogram = [
+                                {"label": "Education", "value": "education_category"},
+                                {"label": "Income", "value": "range_income"}
+                            ]
+
+
+
+vc_content = html.Div(
+    [
+        html.H2("Drop your Subtrip.csv files below"),
+        html.P("This is the text container where you can put your text."),
+        dcc.Upload(
+            ["Drag and Drop or ", html.A("Select a File")],
+            style={
+                "width": "100%",
+                "height": "60px",
+                "lineHeight": "60px",
+                "borderWidth": "1px",
+                "borderStyle": "dashed",
+                "borderRadius": "5px",
+                "textAlign": "center",
+            },
+            id="upload-data",
+            multiple=True,
+        ),
+        html.Div(id="file-list"),
+        html.Button("Process Files", id="transform-button", n_clicks=0),
+        html.Hr(),
+        dcc.Loading(
+            id="loading",
+            type="default",
+            children=[
+                html.Div(
+                    [
+                        
+                        html.Div(id="visualization-1", style={"display": "none"}),
+                        dbc.Row(
+                           [
+                               dbc.Col(html.Div(id="fig-tours-container")),
+                               dbc.Col(html.Div("One of three columns")),
+                           ] 
+                        ),
+                        dbc.Row(
+                            [
+                                dbc.Col(
+                                    dcc.Dropdown(
+                                        id="color-dropdown",
+                                        options=dropdown_options_histogram,
+                                        value="education_category",
+                                        clearable=False,
+                                        style={"display": "none"},
+                                    )
+                                ),
+                                html.Div(id="fig-trips-container")# Add this line
+                            ]
+                        ),
+                        html.Iframe(id='map-iframe', src='',height='1000', width='1000'),
+                    ]
+                )
+            ],
+        ),
+        html.Div(id="alert-message"),
+    ],
+    id="page-content",
+    className="vc-content",
+)
+
+
+@app.callback(
+    [
+        Output("visualization-1", "children"),
+        Output("visualization-1", "style"),
+        Output("color-dropdown", "style"),
+        Output("alert-message", "children"),
+        Output("fig-tours-container", "children"),
+        Output("fig-trips-container", "children"),
+        Output('map-iframe', 'srcDoc')
+    ],
+    [
+        Input("transform-button", "n_clicks"),
+        Input("color-dropdown", "value"),
+    ],
+    [State("upload-data", "contents")],
+    prevent_initial_call=True,
+)
+def process_uploaded_files(n_clicks, color_value, contents):
+    if contents is None or len(contents) == 0:
+        return (
+            [],
+            {"display": "none"},
+            {"display": "none"},
+            html.Div("Please upload files before processing."),
+            None,
+            None,
+            '',
+        )
+    else:
+        combined_data = process_files(contents)
+
+        if combined_data is not None:
+            fig_trips = trips_day(combined_data)
+            fig_tour = tours_person(combined_data)
+            fig = create_histogram_plot(combined_data, color_value)
+            
+            zones = gpd.read_file('data/shp_files/sm_zone.shp')
+            zones.to_crs(epsg=4326)
+            
+            trips_df = combined_data[['person_id','start_time','travel_time','origin_lat','origin_long','destination_lat','destination_long']]
+
+            trips_df['start_time'] = trips_df['start_time'].dt.strftime('%Y-%m-%d %H:%M:%S') 
+            
+            config = {}
+            exec(open("hex_config.py").read(), config)
+            config = config["config"]
+            map_1 = KeplerGl(height=800, data={'zones': zones, 'trips_df': trips_df}, config=config)
+            map_1.save_to_html(file_name="map_kepler.html")
+
+            with open('map_kepler.html', 'r') as f:
+                kepler_html = f.read()
+            
+            return [
+                dcc.Graph(figure=fig_trips),
+                {"display": "block"},
+                {"display": "block"},
+                "",
+                dcc.Graph(figure=fig_tour),
+                dcc.Graph(figure=fig),
+                kepler_html,
+            ]
+        else:
+            return (
+                [],
+                {"display": "none"},
+                {"display": "none"},
+                html.Div("No file uploaded yet."),
+                None,
+                None,
+                '',
             )
 
 
 
 @app.callback(
-    [
-        Output('visualization-1', 'children'),
-        Output('visualization-1', 'style'),
-        Output('color-dropdown', 'style'), 
-        Output('alert-message', 'children'),
-    ],
-    [
-        Input('transform-button', 'n_clicks'),
-        Input('color-dropdown', 'value')  # Add this line to get the dropdown value
-    ],
-    [State('upload-data', 'contents')],
-    prevent_initial_call=True
-)
-def process_uploaded_files(n_clicks, color_value, contents):
-    if contents is None or len(contents) == 0:
-        return [], {'display': 'none'}, {'display': 'none'}, html.Div('Please upload files before processing.')
-    else:
-        combined_data = process_files(contents)
-
-        if combined_data is not None:
-            fig = create_histogram_plot(combined_data, color_value)
-            return [
-                dcc.Graph(figure=fig),
-                {'display': 'block'},
-                {'display': 'block'},
-                ''
-            ]
-        else:
-            return [], {'display': 'none'}, {'display': 'none'}, html.Div('No file uploaded yet.')
-
-
-@app.callback(
-    Output('file-list', 'children'),
-    [Input('upload-data', 'filename')],
-    prevent_initial_call=True
+    Output("file-list", "children"),
+    [Input("upload-data", "filename")],
+    prevent_initial_call=True,
 )
 def update_file_list(filenames):
     if filenames:
-        return html.Ul([
-            html.Li(filename) for filename in filenames
-        ])
+        return html.Ul([html.Li(filename) for filename in filenames])
     else:
-        return html.Div('No files selected.')
-
-
-
-
+        return html.Div("No files selected.")
 
 
 app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
@@ -212,10 +343,12 @@ app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
 @app.callback(Output("page-content", "children"), Input("url", "pathname"))
 def render_page_content(pathname):
     if pathname == "/":
+        return main_page
+    elif pathname == "/Virtual_City":
         return vc_content
-    elif pathname == "/calendar":
+    elif pathname == "/Boston":
         return html.P("This is your calendar... not much in the diary...")
-    elif pathname == "/messages":
+    elif pathname == "/Singapore":
         return html.P("Here are all your messages")
     # If the user tries to reach a different page, return a 404 message
     return html.Div(
--- visualization.py
+++ visualization.py
@@ -2,40 +2,124 @@ import numpy as np
 import plotly.express as px
 import pandas as pd
 
+
+
+
+def trips_day(combined_data):
+    trip_group =  combined_data.groupby(['label','time_range'])['count'].sum().reset_index()
+    # Calculate the total population count
+    total_trip = trip_group['count'].sum()
+
+    # Add a percentage column
+    trip_group['Percentage'] = (trip_group['count'] / total_trip) * 100
+
+
+    # Create a bar plot using Plotly
+    fig_trips = px.bar(trip_group, x='time_range', y='Percentage', color='label',
+                labels={'time_range': 'Time of day (30 min time intervals)', 'Percentage': 'Percentage (%)', 'label':'Scenario'},
+                barmode='group',
+                title='Trips by Time of Day',
+                template='plotly',
+                hover_data=['count', 'Percentage'])
+
+
+    fig_trips.update_layout(legend=dict(
+        orientation="h",
+        yanchor="bottom",
+        y=1.02,
+        xanchor="right",
+        x=1
+    ))
+    return fig_trips
+
+
+def tours_person(combined_data):
+    # Group by 'person_id' and 'label', and sum the 'count' column
+    number_tours = combined_data.groupby(['person_id', 'label'])['count'].sum().reset_index()
+
+    # Group by 'count' and 'label', and count the occurrences
+    number_tours = number_tours.groupby(['count', 'label']).size().reset_index(name='Population_Count')
+
+    # Replace values greater than 4 with '+4 trips' in a new column 'trip_count'
+    number_tours['trip_count'] = pd.cut(number_tours['count'], bins=[1, 2, 3, 4, 5,float('inf')],
+                                    labels=['1', '2', '3', '4', '+4 trips'], right=False)
+
+    # Group by 'label' and 'trip_count', and sum the 'Population_Count' column
+    number_tours = number_tours.groupby(['label', 'trip_count'])['Population_Count'].sum().reset_index()
+
+    # Define the order of the 'trip_count' categories
+    category_order = ['1', '2', '3', '4', '+4 trips']
+
+    # Convert 'trip_count' to categorical with the defined order
+    number_tours['trip_count'] = pd.Categorical(number_tours['trip_count'], category_order)
+
+    # Sort the DataFrame based on the categorical order
+    number_tours = number_tours.sort_values('trip_count')
+
+    # Calculate the total population count
+    total_population = number_tours['Population_Count'].sum()
+
+    # Add a percentage column
+    number_tours['Percentage'] = (number_tours['Population_Count'] / total_population) * 100
+    
+        # Create a bar plot using Plotly
+    fig_tour = px.bar(number_tours, x='trip_count', y='Percentage', color='label',
+                labels={'trip_count': 'Number of Tours', 'Percentage': 'Percentage (%)', 'label':'Scenario'},
+                barmode='group',
+                title='Number of Tours per Person',
+                template='plotly',
+                hover_data=['Population_Count', 'Percentage'])
+
+
+    fig_tour.update_layout(legend=dict(
+        orientation="h",
+        yanchor="bottom",
+        y=1.02,
+        xanchor="right",
+        x=1
+    ))
+    
+    return fig_tour
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def create_histogram_plot(combined_data, color_value):
-    
-    
-    
-    x_max = np.max(combined_data['time_range'])
-            
-    fig = px.histogram(combined_data, 
-                       facet_col='label',
-                       title="Distribution of Trips Across Scenarios",
-                       x="time_range", 
-                       color=color_value,
-                       marginal="box",
-                       nbins=20)
-            
-    fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
-            
+    if color_value == 'education_category':
+        education_group = combined_data.groupby(['label', 'start_time', 'education_category'])['count'].sum().reset_index()
+        fig = px.histogram(education_group,
+                           facet_col='label',
+                           title='Distribution of Trips by Education',
+                           x='start_time',
+                           y='count',
+                           color='education_category',
+                           )
+    elif color_value == 'range_income':
+        income_group = combined_data.groupby(['label', 'start_time', 'range_income'])['count'].sum().reset_index()
+        fig = px.histogram(income_group,
+                           facet_col='label',
+                           title='Distribution of Trips by Education',
+                           x='start_time',
+                           y='count',
+                           color='range_income',
+                           )
+
     fig.update_layout(height=600) 
 
-    # Check if the maximum value of x-axis data is greater than the threshold
-    if x_max >= pd.Timestamp("1900-01-01 09:30:00"):
-        fig.add_vrect(x0="1900-01-01 07:30:00", x1="1900-01-01 09:30:00",
-                      annotation_text="Morning Peak",
-                      fillcolor="red", opacity=0.25, line_width=0)
-
-    if x_max >= pd.Timestamp("1900-01-01 14:00:00"):
-        fig.add_vrect(x0="1900-01-01 11:00:00", x1="1900-01-01 14:00:00",
-                      annotation_text="Off Peak",
-                      fillcolor="green", opacity=0.25, line_width=0)
-
-    if x_max >= pd.Timestamp("1900-01-01 18:30:00"):
-        fig.add_vrect(x0="1900-01-01 17:00:00", x1="1900-01-01 18:30:00",
-                      annotation_text="Evening Peak",
-                      fillcolor="orange", opacity=0.25, line_width=0)
-    
     return fig
 
 
